f39fb83a8014e8ee5554d59885b911cc9fbf3955
mickaelquint <nasribery13@gmail.com>
2019-02-13 12:35:05 +0100

    exoencours

---
+++ 08-superglobal/contact.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css"
+        integrity="sha384-PDle/QlgIONtM1aqA2Qemk5gPOE7wFq8+Em+G/hmo5Iq0CCmYZLv3fVRDJ4MMwEA" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/js/bootstrap.min.js"
+        integrity="sha384-7aThvCh9TypR7fIc2HV4O/nFMVCBwyIUKL8XCtKE+8xgCgl/PQGuFsvShjr74PBp" crossorigin="anonymous">
+    </script>
+
+</head>
+
+<body>
+    <?php
+
+
+?>
+    <form form action="" method="post">
+
+        <div class="form-group row">
+            <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" id="staticEmail" placeholder="email@example.com">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="inputsujet" class="col-sm-2 col-form-label">sujet</label>
+            <div class="col-sm-10">
+                <input type="sujet" class="form-control" id="inputsujet" placeholder="sujet">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="inputmessage" class="col-sm-2 col-form-label">message</label>
+            <div class="col-sm-10">
+                <input type="message" class="form-control" id="inputmessage" placeholder="message">
+            </div>
+        </div>
+    </form>
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+
+</body>
+
+</html>
